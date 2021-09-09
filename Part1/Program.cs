9f4b99c4e8ed72950f98a61a0d786fc659a48897
spostew <spencerunitt0@gmail.com>
2021-09-08 17:23:42 -0700

    First programs sent to Github

---
+++ Part1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+    Part!: This program will take a standard name and convert it to pig latin
+    Name: Spencer Unitt
+    Module: Homework 2, Part1
+    Problem Statement: Use pig latin to alter your first and last name
+    Algorithm:
+        1. Create two strings named first and last and set them = to your name
+        2. Move the first char from the first string and last string to the back
+        3. In a new string named full, concatinate the strings together and set the
+           first char to uppercase
+        4. Write the result to the console.
+        
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Strings used in program
+            string first_name = "spencer";
+            string last_name = "unitt";
+            string pig = "ay";
+            string full_name;
+
+            // Moves the first char in the names to the back and adds ay to the end
+            first_name = first_name.Substring(1, 6) + first_name.Substring(0, 1) + pig;
+            last_name = last_name.Substring(1, 4) + last_name.Substring(0, 1) + pig;
+
+            // Combines the strings together and converts the first char to uppercase
+            full_name = char.ToUpper(first_name[0]) + first_name.Substring(1) + " " + char.ToUpper(last_name[0]) + last_name.Substring(1);
+           
+            // Sends the results to the console
+            Console.WriteLine(full_name);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+    Part2: This program will convert the temp the user enters from F to C
+    Name: Spencer Unitt
+    Module: Homework 2, Part2
+    Problem Statement: Show how to convert tempurature by using user input.
+    Algorithm:
+        1. Declare a double variable named degreeC
+        2. Create a welcome message for user and prompt for a tempuarature
+        3. Set a string called temp to the next line displayed in the console
+        4. Use the equation degreeC = 5 * (double.Parse(temp) - 32) / 9 to convert
+        5. Send the results back to the user
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            double degreeC;
+            // Prompts user for some input
+            Console.WriteLine("Enter in a tempurature below: ");
+            // Takes the input entered from the user
+            string temp = Console.ReadLine();
+            
+            // converts the int the user entered into a double
+            degreeC = 5 * (double.Parse(temp) - 32) / 9;
+
+            // displays the message to the user to 1 decimal place
+            Console.WriteLine("The tempurature in Celsius is: " + Math.Round(degreeC, 1));
+            Console.ReadLine();
+
+        }
+    }
+}
